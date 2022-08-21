b05c343e3d35ca518ed7365071b2002fffa3d1f2
brian rodrigo brown coopman <brianbrowncoopman@gmail.com>
2022-08-21 12:21:58 -0400

    12:21 12-8

---
+++ spinner.js
@@ -0,0 +1,11 @@
+let c = console.log;
+c('hola BBC')
+
+
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector('.loader')
+  loader.getElementsByClassName.visibility = 'hidden'
+
+})
+
